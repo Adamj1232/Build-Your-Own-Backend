e6e7152e5757817344c094f3a4b618a7f01f4e49
Adam Carpenter <adamcarpenter86@gmail.com>
2017-07-15 15:57:17 -0600

    updated test file to reflect smaller test data set

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -118,6 +118,7 @@ describe("API Routes", () => {
       chai.request(server)
       .get("/api/v1/venues/cities/name/arvada")
       .end((err, response) => {
+        console.log(response);
         response.should.have.status(200);
         response.should.be.json;
         response.body.should.be.a("array");
